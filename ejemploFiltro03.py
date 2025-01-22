a359876601c249be739a1afff9983ff0c2f2b736
diego <diegoan1624@gmail.com>
2025-01-22 00:19:50 -0500

    comentando filtros

--- ejemploFiltro03.py
+++ ejemploFiltro03.py
@@ -1,13 +1,23 @@
-#filtro otsu
-import cv2
+# Filtro Otsu
+import cv2  # Importar la biblioteca OpenCV para procesamiento de imágenes
 
-img = cv2.imread('C:/Users/diego/Desktop/IA/ConjuntoImagenes/Ejemplo01.jpg',0)
+# Leer la imagen en escala de grises desde la ruta especificada
+img = cv2.imread('C:/Users/diego/Desktop/IA/ConjuntoImagenes/Ejemplo01.jpg', 0)
 
+# Aplicar el método de umbralización de Otsu
+# cv2.threshold toma la imagen, un umbral inicial (0), el valor máximo (255) y el tipo de umbral (THRESH_OTSU)
+# Devuelve el umbral calculado y la imagen umbralizada
 umbral, umbralizado = cv2.threshold(img, 0, 255, cv2.THRESH_OTSU)
+
+# Mostrar la imagen umbralizada en una ventana
 cv2.imshow('img filtrada', umbralizado)
+
+# Imprimir el umbral calculado por el método de Otsu en la consola
 print(f"El umbral calculado por Otsu es: {umbral}")
+
+# Mostrar la imagen original en una ventana
 cv2.imshow('imagen original', img)
 
-
+# Esperar a que se presione una tecla y cerrar todas las ventanas
 cv2.waitKey(0)
 cv2.destroyAllWindows()
